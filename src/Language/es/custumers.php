22dbb652c91d2c945f9b7a88a56a4d6f06411eb9
Julio Cesar Leyva Rodriguez <juliocesarleyvarodriguez@hotmail.com>
2024-12-02 12:55:30 -0700

    first commit

---
+++ src/Language/es/custumers.php
@@ -0,0 +1,28 @@
+ <?php
+$custumers["logDescription"] = "El registro en clientes fue guardado con los siguientes datos:";
+$custumers["logUpdate"] = "El registro en clientes fue actualizado con los siguientes datos:";
+$custumers["logDeleted"] = "El registro en clientes fue eliminado con los siguientes datos:";
+$custumers["msg_delete"] = "El Registro en clientes fue eliminado correctamente:";
+$custumers["add"] = "Agregar Cliente";
+$custumers["edit"] = "Editar Cliente";
+$custumers["createEdit"] = "Crear / Editar";
+$custumers["title"] = "Admon. Clientes";
+$custumers["subtitle"] = "Lista de Clientes";
+$custumers["fields"]["firstname"] = "Nombre";
+$custumers["fields"]["lastname"] = "Apellido";
+$custumers["fields"]["taxID"] = "RFC";
+$custumers["fields"]["email"] = "Correo Electronico";
+$custumers["fields"]["direction"] = "Direccion";
+$custumers["fields"]["birthdate"] = "Fecha de nacimiento";
+$custumers["fields"]["created_at"] = "Fecha de creacion";
+$custumers["fields"]["updated_at"] = "Ultima modificacion";
+$custumers["fields"]["deleted_at"] = "Fecha de eliminacion";
+
+ $custumers["fields"]["actions"] = "Acciones";       
+$custumers["msg"]["msg_insert"] = "Registro agregado correctamente.";
+$custumers["msg"]["msg_update"] = "Registro modificado correctamente.";
+$custumers["msg"]["msg_delete"] = "Registro eliminado correctamente.";
+$custumers["msg"]["msg_get"] = "Registro obtenido correctamente.";
+$custumers["msg"]["msg_get_fail"] = "Registro no encontrado o eliminado.";
+return $custumers;
+        
