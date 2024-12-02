a6442bfe921ec97764dd3ad988cae437b783d2f8
Julio Cesar Leyva Rodriguez <juliocesarleyvarodriguez@hotmail.com>
2024-12-02 15:59:24 -0700

    Changes in the routes

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -3,82 +3,33 @@
 
 $routes->group('admin', function ($routes) {
 
-    $routes->resource('categorias', [
-        'filter' => 'permission:categorias-permission',
-        'controller' => 'categoriasController',
-        'except' => 'show', 
-        'namespace' => 'julio101290\boilerplateproducts\Controllers'
+
+
+    $routes->resource('custumers', [
+        'filter' => 'permission:custumers-permission',
+        'controller' => 'custumersController',
+        'except' => 'show'
+        ,'namespace' => 'julio101290\boilerplatecustumers\Controllers'
     ]);
 
-    $routes->post('categorias/save'
-    , 'CategoriasController::save'
-    ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
+    $routes->post('custumers/save'
+    , 'CustumersController::save'
+    ,['namespace' => 'julio101290\boilerplatecustumers\Controllers']
+    );
+
+    $routes->post('custumers/getCustumers'
+    , 'CustumersController::getCustumers'
+    ,['namespace' => 'julio101290\boilerplatecustumers\Controllers']
     );
     
-    $routes->post('categorias/getCategorias'
-    , 'CategoriasController::getCategorias'
-    ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
+    $routes->post('custumers/getCustumersAjax'
+    , 'CustumersController::getCustumersAjax'
+    ,['namespace' => 'julio101290\boilerplatecustumers\Controllers']
     );
 
-    $routes->post('categorias/buscarFolio'
-        , 'CategoriasController::getFolio'
-        ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
-        );
-
-    $routes->post('categorias/getCategoriasAjax'
-    , 'CategoriasController::getCategoriasAjax'
-    ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
+    $routes->post('custumers/getCustumersTodosAjax'
+    , 'CustumersController::getCustumersTodosAjax'
+    ,['namespace' => 'julio101290\boilerplatecustumers\Controllers']
     );
-
-
-    $routes->resource('products', [
-        'filter' => 'permission:products-permission'
-        ,'controller' => 'productsController'
-        ,'except' => 'show'
-        ,'namespace' => 'julio101290\boilerplateproducts\Controllers'
-       
-    ]);
-
-    $routes->post('products/save'
-    , 'ProductsController::save'
-    ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
-    );
-
-    $routes->post('products/getProducts'
-    , 'ProductsController::getProducts'
-     ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
-    );
-
-
-    $routes->get('products/getAllProducts/(:any)'
-    , 'ProductsController::getAllProducts/$1'
-    ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
-    );
-
-    $routes->get('products/barcode/(:any)'
-    , 'ProductsController::getBarcodePDF/$1'
-    ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
-    );
-
-    $routes->get('products/getAllProductsInventory/(:any)/(:any)/(:any)'
-    , 'ProductsController::getAllProductsInventory/$1/$2/$3'
-    ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
-    );
-
-    $routes->post('products/getUnidadSATAjax'
-            , 'ProductsController::getUnidadSATAjax'
-            ,['namespace' => 'julio101290\boilerplateproducts\Controllers']);
-
-
-    $routes->post('products/getProductosSATAjax'
-    , 'ProductsController::getProductosSATAjax'
-    ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
-    );
-
-    $routes->post('products/getProductsAjax'
-    , 'ProductsController::getProductsAjaxSelect2'
-    ,['namespace' => 'julio101290\boilerplateproducts\Controllers']
-    );
-
 
 });
